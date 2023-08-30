326a85a6c9aa9f8dbe7275ac71dd6db2f5a7ac81
Hanmin Park <hanmin.01.park@gmail.com>
2023-08-30 12:07:17 +0200

    ✨🫧 First ex: done

--- CPP_Module_00/ex00/megaphone.cpp
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -6,7 +6,36 @@
 /*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 16:18:54 by hanmpark          #+#    #+#             */
-/*   Updated: 2023/08/28 16:18:56 by hanmpark         ###   ########.fr       */
+/*   Updated: 2023/08/30 12:04:15 by hanmpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cstring>
+
+std::string	make_loud_noise(std::string simple_noise) {
+
+	for (size_t i = 0; i < simple_noise.length(); i++) {
+		simple_noise[i] = toupper(simple_noise[i]);
+	}
+
+	return simple_noise;
+}
+
+int	main(int argc, char **argv) {
+
+	std::string	noise;
+
+	if (argc < 2) {
+		noise = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	else {
+		for (int i = 1; i < argc; i++) {
+			noise += make_loud_noise(argv[i]);
+		}
+	}
+
+	std::cout << noise << std::endl;
+
+	return 0;
+}
