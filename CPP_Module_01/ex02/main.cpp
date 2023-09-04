d9d08d73ff064a251bca7d07bab31ca8f3d51f50
Hanmin Park <hanmin.01.park@gmail.com>
2023-09-04 23:25:09 +0200

    ✨🫧 ex02: done

---
+++ CPP_Module_01/ex02/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 23:14:44 by hanmpark          #+#    #+#             */
+/*   Updated: 2023/09/04 23:24:24 by hanmpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void) {
+
+	std::string	stringBrain = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &stringBrain;
+	std::string	&stringREF = stringBrain;
+
+	/* Address of string */
+	std::cout << "Address of string in memory: " << &stringBrain << std::endl;
+	std::cout << "Address stocked in stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address stocked in stringREF: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	/* Value of string */
+	std::cout << "Value of string: " << stringBrain << std::endl;
+	std::cout << "Value pointed by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed by stringREF: " << stringREF << std::endl;
+
+	return 0;
+}
