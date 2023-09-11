1f5215b55f7e54d159b66cd3ea2dee4a99442ab2
Hanmin Park <hanmin.01.park@gmail.com>
2023-09-11 19:59:41 +0200

    ✨🌱 ex01: done

---
+++ CPP_Module_02/ex01/inc/Fixed.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 11:53:50 by hanmpark          #+#    #+#             */
+/*   Updated: 2023/09/11 19:23:01 by hanmpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class	Fixed {
+
+public:
+	Fixed(void);
+	Fixed(Fixed const &rhs);
+	Fixed	&operator=(Fixed const &rhs);
+	~Fixed(void);
+
+	Fixed(int const d);
+	Fixed(float const f);
+
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+
+	float	toFloat(void) const;
+	int		toInt(void) const;
+
+private:
+	int					_fixedPointValue;
+	static int const	_fractionalBits = 8;
+
+};
+
+std::ostream	&operator<<(std::ostream &o, Fixed const &fp);
+
+#endif
---
+++ CPP_Module_02/ex01/src/Fixed.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 11:54:08 by hanmpark          #+#    #+#             */
+/*   Updated: 2023/09/11 19:28:39 by hanmpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+#include <cmath>
+
+Fixed::Fixed(void) : _fixedPointValue(0) {
+
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &rhs) {
+
+	std::cout << "Copy constructor called" << std::endl;
+	*this = rhs;
+}
+
+Fixed::Fixed(int const d) : _fixedPointValue(d * (1 << _fractionalBits)) {
+
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(float const f) : _fixedPointValue(std::roundf(f * (1 << _fractionalBits))) {
+
+	std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed	&Fixed::operator=(Fixed const &rhs) {
+
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &rhs) {
+		this->_fixedPointValue = rhs.getRawBits();
+	}
+	return *this;
+}
+
+Fixed::~Fixed() {
+
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits(void) const {
+
+	return this->_fixedPointValue;
+}
+
+void	Fixed::setRawBits(int const raw) {
+
+	this->_fixedPointValue = raw;
+}
+
+float	Fixed::toFloat(void) const {
+
+	return static_cast<float>(this->getRawBits()) / (1 << _fractionalBits);
+}
+
+int	Fixed::toInt(void) const {
+
+	return this->getRawBits() / (1 << _fractionalBits);
+}
+
+std::ostream	&operator<<(std::ostream &o, Fixed const &fp) {
+
+	o << fp.toFloat();
+	return o;
+}
