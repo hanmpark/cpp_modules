dfe627b4427a7ec120f1efb8f10cb43285767d06
Hanmin Park <hanmin.01.park@gmail.com>
2023-09-14 15:14:49 +0200

    ✨ Small modifs

--- CPP_Module_02/ex03/src/main.cpp
+++ CPP_Module_02/ex03/src/main.cpp
@@ -6,14 +6,19 @@
 /*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 11:54:17 by hanmpark          #+#    #+#             */
-/*   Updated: 2023/09/14 15:07:21 by hanmpark         ###   ########.fr       */
+/*   Updated: 2023/09/14 15:14:20 by hanmpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
 int	main(void) {
-	if (bsp(Point(0, 0), Point(4, 1), Point(1, 3), Point(1.9, 2.3))) {
+	Point const	a(0, 0);
+	Point const	b(4, 1);
+	Point const	c(1, 3);
+	Point const	d(1.9, 2.3);
+
+	if (bsp(a, b, c, d)) {
 		std::cout << "Point is INSIDE the triangle" << std::endl;
 	} else {
 		std::cout << "Point is OUTSIDE the triangle" << std::endl;
