54b7e3890dd69c078774bb0efce3a8942d80a5aa
hanmpark <hanmin.01.park@gmail.com>
2024-03-18 18:08:43 +0100

    CPP 07 ex00 done

---
+++ CPP_Module_07/inc/whatever.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/18 17:21:09 by hanmpark          #+#    #+#             */
+/*   Updated: 2024/03/18 18:08:06 by hanmpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+
+template<typename T>
+void	swap(T &a, T &b) {
+	T	c = a;
+
+	a = b;
+	b = c;
+}
+
+template<typename T>
+T const	&min(T const &a, T const &b) {
+	if (a < b)
+		return a;
+	return b;
+}
+
+template<typename T>
+T const	&max(T const &a, T const &b) {
+	if (a > b)
+		return a;
+	return b;
+}
+
+#endif
---
+++ CPP_Module_07/src/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/18 17:21:50 by hanmpark          #+#    #+#             */
+/*   Updated: 2024/03/18 18:06:33 by hanmpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int	main() {
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
