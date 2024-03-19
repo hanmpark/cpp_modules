eefb405064aa18b6ffe382a53539864a7099ea6c
hanmpark <hanmin.01.park@gmail.com>
2024-03-19 19:53:34 +0100

    Started CPP08

---
+++ CPP_Module_08/ex00/inc/easyfind.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 18:42:39 by hanmpark          #+#    #+#             */
+/*   Updated: 2024/03/19 18:46:04 by hanmpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+
+template<typename T>
+int	easyfind(T const &container, int integer) {
+	
+}
+
+#endif
