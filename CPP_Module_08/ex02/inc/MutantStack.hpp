65c9dc193ea224492bcab5db690ec5cee9c9dc05
hanmpark <hanmin.01.park@gmail.com>
2024-03-20 18:42:40 +0100

    CPP08 done

---
+++ CPP_Module_08/ex02/inc/MutantStack.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 15:49:39 by hanmpark          #+#    #+#             */
+/*   Updated: 2024/03/20 18:34:33 by hanmpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+# include <deque>
+
+# define PURPLE	"\033[38;5;99m"
+# define GREEN	"\033[38;5;115m"
+# define BOLD	"\033[1m"
+# define DEF	"\033[0m"
+
+template<typename T>
+class MutantStack : public std::stack<T> {
+public:
+	MutantStack<T>() : std::stack<T>() {}
+	MutantStack<T>(MutantStack<T> const &copy) : std::stack<T>(copy) {}
+	virtual ~MutantStack<T>() {}
+	MutantStack<T>	&operator=(MutantStack<T> const &rhs) {
+		return std::stack<T>::operator=(rhs);
+	}
+	typedef typename std::deque<T>::iterator 		iterator;
+	typedef typename std::deque<T>::const_iterator	const_iterator;
+	iterator begin() {
+		return this->c.begin();
+	}
+	iterator end() {
+		return this->c.end();
+	}
+	const_iterator begin() const {
+		return this->c.begin();
+	}
+	const_iterator end() const {
+		return this->c.end();
+	}
+};
+
+#endif
---
+++ CPP_Module_08/ex02/src/main.cpp
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 17:09:27 by hanmpark          #+#    #+#             */
+/*   Updated: 2024/03/20 18:41:33 by hanmpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+#include <list>
+
+int	main() {
+	{
+		std::cout << BOLD PURPLE "Subject main:" DEF << std::endl;
+		std::cout << GREEN "testing out with MutantStack<int>" DEF << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+	{
+		std::cout << std::endl << "--------------------------------------------" << std::endl;
+		std::cout << BOLD PURPLE "Personal main:" DEF << std::endl;
+		std::cout << GREEN "testing out with std::list<int>" DEF << std::endl;
+		std::list<int> mstack;
+		mstack.push_back(5);
+		mstack.push_back(17);
+		std::cout << mstack.back() << std::endl;
+		mstack.pop_back();
+		std::cout << mstack.size() << std::endl;
+		mstack.push_back(3);
+		mstack.push_back(5);
+		mstack.push_back(737);
+		mstack.push_back(0);
+		std::list<int>::iterator it = mstack.begin();
+		std::list<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
+	{
+		std::cout << std::endl << "--------------------------------------------" << std::endl;
+		std::cout << BOLD PURPLE "Personal main:" DEF << std::endl;
+		std::cout << GREEN "testing out with std::deque<int>" DEF << std::endl;
+		std::deque<int> mstack;
+		mstack.push_back(5);
+		mstack.push_back(17);
+		std::cout << mstack.back() << std::endl;
+		mstack.pop_back();
+		std::cout << mstack.size() << std::endl;
+		mstack.push_back(3);
+		mstack.push_back(5);
+		mstack.push_back(737);
+		mstack.push_back(0);
+		std::deque<int>::iterator it = mstack.begin();
+		std::deque<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
+	return 0;
+}
